fa4ab85a6a5748d64fab23411761ac519355c2c1
Pavan <pavannp756@gmail.com>
2026-09-13 19:56:35 +0000

    All Listings and Adding new listing

--- app.js
+++ app.js
@@ -1,17 +1,69 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const Listing=require("./models/Listing")
+const path=require("path");
+
+
 const MONGO_URL="mongodb://127.0.0.1:27017/Wanderlust";
 main().catch(err => console.log(err));
+
 
 async function main() {
     await mongoose.connect(MONGO_URL);
     console.log("MongoDB connected!");
 }
-
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
     res.send("At root directory");
+});
+
+//Index Route
+app.get("/listings",async (req,res)=>{
+    const allListings=await Listing.find({});
+   // console.log(allListings);
+    res.render("listings/index.ejs",{allListings});
+    
+});
+
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//Create new Route
+
+app.post("/listings",async (req,res)=>{
+    
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+    
+
 })
+
+//Show Route
+app.get("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+
+})
+
+// app.get("/ListingTest",async (req,res)=>{
+//         let sample=new Listing({
+//             title:"Beach",
+//             description:"Maya Beach Goa",
+//             price:1500,
+//             location:"Goa",
+//             country:"India",
+
+//         })
+//         await sample.save().catch((err)=>console.log(err));
+//         res.send("Saved Successfuly");
+// })
 app.listen(8080, (req,res)=>{
     console.log("Listening to Port");
 });
